e8dc90b3e1938249d99103c57911e381ea328ee6
Andrea Zagar <andrea@192.168.2.15>
2019-03-31 16:51:01 -0300

    Deleted some pictures, working on captions for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -294,30 +294,6 @@ console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55')
                 preloader: true
             }
         });
-        
-        $('.fourColumns').magnificPopup({
-		  delegate: 'a', // child items selector, by clicking on it popup will open
-		  type: 'image',
-		  // other options
-		  gallery: {enabled: true},
-		  image: {
-		  markup: '<div class="mfp-figure">'+
-		            '<div class="mfp-close"></div>'+
-		            '<div class="mfp-img"></div>'+ // Floated left
-		            '<div class="mfp-title"></div>'+ // This is floated right shows up on the right side
-		            '<div class="mfp-bottom-bar">'+
-		              '<div class="mfp-counter"></div>'+
-		            '</div>'+
-		          '</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
-		
-		  cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor. 
-		
-		  tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
-		  },
-		
-		  preloader: true,
-		
-		});
 
 
         /* ---------------------------------------------- /*
